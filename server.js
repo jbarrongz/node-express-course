f75ecee680b24419a85472837c6ace3504b1df19
Antonio Barron <jbarron@unitedvirtualities.com>
2019-11-29 16:11:39 -0700

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 const mockUserData = [
   {name: 'Mark'},
@@ -21,6 +24,25 @@ app.get('/users/:id', (req, res)=>{
     message: 'Got a user...',
     user: req.params.id
   });
+});
+
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const serverUser = 'antonio';
+  const serverPass = '123456';
+  if (username === serverUser && password === serverPass) {
+    res.json({
+			success: true,
+			message: 'password and username match!',
+			token: 'FWEFregEHtrHETBGE'
+		})
+  } else {
+    res.json({
+			success: false,
+			message: 'password and username do not match'
+		})
+  }
 });
 
 app.listen(8000, () => console.log('The server is running'));
